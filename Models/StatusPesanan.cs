28724272c8a47b39b2982f0e407458133fb31988
Farhan Arief Ramadhan <113919723+FarhanAR09@users.noreply.github.com>
2024-09-05 12:08:19 +0700

    added models Pesanan dan Status

---
+++ Models/StatusPesanan.cs
@@ -0,0 +1,8 @@
+﻿namespace ecycle_be.Models
+{
+    public class StatusPesanan
+    {
+        public int StatusPesananID { get; set; } = -1;
+        public string NamaStatus { get; set; } = string.Empty;
+    }
+}
