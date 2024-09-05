69bf88e9bc73a03cd950792af6959b928ba0471e
Farhan Arief Ramadhan <113919723+FarhanAR09@users.noreply.github.com>
2024-09-05 15:00:51 +0700

    added authController

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using ecycle_be.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
+
+namespace ecycle_be.Controllers
+{
+    [ApiController]
+    [Route("auth")]
+    public class AuthController : Controller
+    {
+        [HttpPatch("login")]
+        public IActionResult Login([FromBody] Pengguna pengguna)
+        {
+            return Ok(new
+            {
+                token = "token",
+                user = pengguna.Nama,
+                role = "admin",
+                pass = pengguna.Password
+            });
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
